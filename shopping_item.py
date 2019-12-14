31dd8716259134c5c24cbad050d57b73c8a553ba
vlad <vladov3000@gmail.com>
2019-12-13 22:19:41 -0800

    first commit

---
+++ shopping_item.py
@@ -0,0 +1,72 @@
+from typing import Dict, List
+
+default_testcases = {
+    "a": [3, 2],
+    "imreallylong---------------": [3.5, 2],
+    "hello world": [3, 2.5],
+    ":)": [3.5, 2.5]
+}
+
+class ShoppingItem:
+    """ A Shopping Item represents one item on the shopping list
+    """
+
+    max_name_length = -1
+    max_quantity_digits = -1
+
+    def __init__(self, name: str, price: float, quantity: float):
+        """ Warns about poor formatting of __str__
+         if name length or digits exceed respective maximums.
+
+        :param name: name of product
+        :param price: price of single product
+        :param quantity: quantity of products
+        """
+        if len(name) > ShoppingItem.max_name_length:
+            ShoppingItem.max_name_length = len(name)
+        if len(str(quantity)) > ShoppingItem.max_quantity_digits:
+            ShoppingItem.max_quantity_digits = len(str(quantity))
+        self.name = name
+        self.price_per = price
+        self.quantity = quantity
+
+    def total_price(self):
+        """ Get total price of item
+
+        :return: quantity * price_per
+        """
+        return self.quantity * self.price_per
+
+    def __str__(self) -> str:
+        """ Formats to string:
+         * name
+         * price
+         * quantity
+         * total price
+
+        :return: formatted
+        """
+        str_format = "Name: {:<" + str(ShoppingItem.max_name_length) + "} "
+        str_format += "Price Per Item: {:<5} "
+        str_format += "Quantity: {:<" + str(ShoppingItem.max_quantity_digits) + "} "
+        str_format += "Total Price: {:<5} "
+        return str_format.format(self.name, self.price_per, self.quantity, self.total_price())
+
+
+def tests(testcases: Dict[str, List[float]] = default_testcases):
+    """ Constructs and prints testcases as Shopping Items
+
+    :param testcases: Defaults to default_testcases
+        Key: item name
+        Value: list of shopping item parameters.
+    """
+    shopping_items = []
+    for key, val in testcases.items():
+        shopping_items.append(ShoppingItem(key, val[0], val[1]))
+
+    for i in shopping_items:
+        print(i)
+
+
+if __name__ == "__main__":
+    tests()
---
+++ shopping_list.py
@@ -0,0 +1,28 @@
+from typing import List, Dict
+from shopping_item import ShoppingItem, default_testcases
+
+class ShoppingList:
+
+    def __init__(self, shopping_items: List[ShoppingItem]):
+        self.shopping_items = shopping_items
+
+    def __str__(self):
+        for i in self.shopping_items:
+            print(i)
+
+
+def tests(testcases: Dict[str, List[float]] = default_testcases):
+    """ Constructs and prints a Shopping List
+
+    :param testcases: Defaults to default_testcases
+        Key: item name
+        Value: list of shopping item parameters.
+    """
+    shopping_items = []
+    for key, val in testcases.items():
+        shopping_items.append(ShoppingItem(key, val[0], val[1]))
+
+    print(ShoppingList(shopping_items))
+
+if __name__ == "__main__":
+    tests()
